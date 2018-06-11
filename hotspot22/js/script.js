71f09beebabd6942c0c5c4e1106308b6d4ef009b
Bakil Almuntaser <bakil.almuntaser.ba@gmail.com>
2018-06-11 09:57:03 +0300

    As last modify

---
+++ hotspot22/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(e) { 
+	$(".ft").funnyText({
+		speed: 500,
+		borderColor: 'blue',
+		activeColor: 'red',
+		color: 'green',
+		fontSize: '30px',
+		direction: 'both'
+	});
+	
+ } );
